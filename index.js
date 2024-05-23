3b931fb5b27f6b44bb4a2b2427230b686ad67ca8
Haitham <haitham.6767@gmail.com>
2024-05-23 04:29:01 +0300

    compelete jokes API

--- index.js
+++ index.js
@@ -73,28 +73,52 @@ app.put("/jokes/:id", (req, res) => {
 //6. PATCH a joke
 
 app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
 
-  const id = parseInt(req.params.id); 
-
-  const exisitingJoke = jokes.find((joke) => joke.id === id); 
+  const exisitingJoke = jokes.find((joke) => joke.id === id);
 
   const replacementJoke = {
-    id : id,
+    id: id,
     jokeText: req.body.jokeText || exisitingJoke.jokeText,
-    jokeType: req.body.jokeType || exisitingJoke.jokeType
-  }
+    jokeType: req.body.jokeType || exisitingJoke.jokeType,
+  };
 
   const searchIndex = jokes.findIndex((joke) => joke.id === id);
   jokes[searchIndex] = replacementJoke;
   console.log(jokes[searchIndex]);
   res.json(jokes[searchIndex]);
-
-
-})
+});
 
 //7. DELETE Specific joke
 
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No jokes were deleted.` });
+  }
+});
+
 //8. DELETE All jokes
+
+//DELETE All jokes
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `You are not authorised to perform this action.` });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
